c5ab75d0fc010f8d428c948edc19f88f58481134
Gijs <gijs.nagtegaal@hva.nl>
2026-03-18 09:30:42 +0100

    All changes transferrred

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,11 +1,25 @@
-// URL Cleanup logic for the Popover API
+const back = document.querySelector('.back');
+
+back.addEventListener('click', (event) => {
+
+    event.preventDefault();
+
+    if (window.history.length > 1) {
+        window.history.back();
+    }
+});
+
+
+
+
+// URL Cleanup logic for the Popovers
 document.querySelectorAll('.opdracht-popover').forEach(popover => {
     popover.addEventListener('toggle', (event) => {
         if (event.newState === 'closed') {
             // Removes the #hash from the URL when the user closes the popover
             history.replaceState(null, document.title, window.location.pathname + window.location.search);
             
-            // OPTIONAL: Reset the slider to the first question when closed
+            // Reset the slider to the first question when closed
             popover.querySelector('ul').scrollTo({ left: 0 });
         }
     });
